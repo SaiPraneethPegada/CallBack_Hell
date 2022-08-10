aa3dcb77a7a5cc1f0221cfa61151711ffe9ae334
Pegada Sai Praneeth <saipraneethpegada@gmail.com>
2022-08-10 15:53:12 +0530

    script update

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-var div = document.getElementById('timer');
+document.body.innerHTML = `<div class="container">
+<div id="timer"></div>
+</div>`;
 
-setTimeout(() => {div.innerHTML = 10
-        setTimeout(() => {div.innerHTML = 9
-            setTimeout(() => {div.innerHTML = 8
-                setTimeout(() => {div.innerHTML = 7
-                    setTimeout(() => {div.innerHTML = 6
-                        setTimeout(() => {div.innerHTML = 5
-                            setTimeout(() => {div.innerHTML = 4
-                                setTimeout(() => {div.innerHTML = 3
-                                    setTimeout(() => {div.innerHTML = 2
-                                        setTimeout(() => {div.innerHTML = 1
-                                                setTimeout(() => {div.innerHTML = "HAPPY INDEPENDENCE DAY"
-                                            },1000);
-                                        },1000)
-                                    },1000)
-                                },1000)
-                            },1000)
-                        },1000);
-                    },1000);
-                },1000);
-            },1000);
-        },1000);
-},1000);
+var div = document.getElementById("timer");
+
+setTimeout(() => {
+  div.innerHTML = 10;
+  setTimeout(() => {
+    div.innerHTML = 9;
+    setTimeout(() => {
+      div.innerHTML = 8;
+      setTimeout(() => {
+        div.innerHTML = 7;
+        setTimeout(() => {
+          div.innerHTML = 6;
+          setTimeout(() => {
+            div.innerHTML = 5;
+            setTimeout(() => {
+              div.innerHTML = 4;
+              setTimeout(() => {
+                div.innerHTML = 3;
+                setTimeout(() => {
+                  div.innerHTML = 2;
+                  setTimeout(() => {
+                    div.innerHTML = 1;
+                    setTimeout(() => {
+                      div.innerHTML = "HAPPY INDEPENDENCE DAY";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
